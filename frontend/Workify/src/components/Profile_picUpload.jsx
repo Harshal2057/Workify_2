077082c53213caa3ae52718178a36088d39e9b31
Harshal2057 <harshcbhandarkar@gmail.com>
2025-04-23 10:21:27 +0530

    little frontend added to pic upload

--- frontend/Workify/src/components/Profile_picUpload.jsx
+++ frontend/Workify/src/components/Profile_picUpload.jsx
@@ -75,24 +75,24 @@ const Profile_picUpload = () => {
 
   return (
     <div className="mt-20 w-full lg:h-screen flex justify-center items-center">
-      <div className="border-1 border-solid border-black w-4/6 lg:w-3/6 p-2 text-center items-center lg:text-left">
+      <div className="border-1 border-solid border-black w-4/6 lg:w-3/6 p-2 text-center items-center lg:text-left rounded-2xl shadow-2xl" >
         <div>
-          <p className="font-semibold text-2xl">Upload Profile Pic</p>
+          <p className="font-semibold text-2xl"> Profile Pic</p>
         </div>
         
-        <div className="lg:flex justify-center">
-          <div className="flex justify-center lg:block border-3 border-solid border-red-500 rounded-full">
+        <div className="lg:flex flex-col justify-center items-center">
+          <div className="flex justify-center  w-max lg:block border-3 border-solid border-red-500 rounded-full">
             <img src={ image || freelancer?.profilePicUrl || assets.default_profile_icon} alt="" className="rounded-full w-[250px] h-[250px] " />
           </div>
           
-          <div className="flex flex-col gap-3 items-center mt-5 mx-auto lg:items-start lg:mx-0 lg:max-h-max  border-1 border-solid border-black">
-            <button className="border-1 border-solid border-black p-1 px-2 rounded-full hover:bg-black hover:text-white" onClick={handleButton} disabled={isLoading}>
+          <div className="flex flex-col lg:flex-row justify-between gap-3 w-2/6 items-center mt-5 mx-auto lg:items-start lg:mx-0 lg:max-h-max ">
+            <button className="border-3 border-solid border-black p-1 px-2 rounded-full hover:bg-black hover:text-white" onClick={handleButton} disabled={isLoading}>
               <p className="font-semibold">Upload</p>
             </button>
 
             <input type="file"  accept="image/*" className="hidden" ref={inputRef} onChange={handleChangeImg} />
             
-            <button className="border-1 border-solid border-black p-1 rounded-full  hover:bg-black hover:text-white" onClick={handleApply} disabled={isLoading}>
+            <button className="border-3 border-solid border-black p-1 w-max rounded-full  hover:bg-black hover:text-white" onClick={handleApply} disabled={isLoading}>
               <p className="font-semibold">{isLoading ? "Updating.." : "Apply Changes"}</p>
             </button>
           </div>
